02be1e3c6b7c194a5f8d7e18b5edd1170bff58aa
AkramCoder <54069025+AkramCoder@users.noreply.github.com>
2019-12-04 02:51:24 -0500

    Update models.py

--- hello/models.py
+++ hello/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class names(models.Model):
     name = models.CharField(max_length=150)
-    lastname = models.CharField(max_length=150, default='akeblersane')
+    lastname = models.CharField(max_length=150)
     
     def __str__(self):
         return self.name
